c5e253bea660fad58f070f316f09f030a9f4e2b5
ThreeSRR <976005003@qq.com>
2023-05-27 22:19:35 +0800

    remove unnecessary files

--- scripts/run_alexnet_feature_size.sh
+++
@@ -1,7 +0,0 @@
-CUDA_VISIBLE_DEVICES=1 python 1_train_feature_size.py --work_path ./experiments/cifar10/1_feature_size/alexnet_sgd/16-8-4-2-1
-CUDA_VISIBLE_DEVICES=1 python 1_train_feature_size.py --work_path ./experiments/cifar10/1_feature_size/alexnet_sgd/16-8-4-4-1
-CUDA_VISIBLE_DEVICES=1 python 1_train_feature_size.py --work_path ./experiments/cifar10/1_feature_size/alexnet_sgd/16-8-8-4-1
-CUDA_VISIBLE_DEVICES=1 python 1_train_feature_size.py --work_path ./experiments/cifar10/1_feature_size/alexnet_sgd/4-2-2-2-1
-CUDA_VISIBLE_DEVICES=1 python 1_train_feature_size.py --work_path ./experiments/cifar10/1_feature_size/alexnet_sgd/8-4-2-2-1
-CUDA_VISIBLE_DEVICES=1 python 1_train_feature_size.py --work_path ./experiments/cifar10/1_feature_size/alexnet_sgd/8-4-4-2-1
-CUDA_VISIBLE_DEVICES=1 python 1_train_feature_size.py --work_path ./experiments/cifar10/1_feature_size/alexnet_sgd/8-4-4-4-1
